355cc7262b4f30bcc3c40612c12df6213f1cf0fa
Joshua Karanja <jokaranja26@gmail.com>
2025-12-06 13:20:49 +0300

    stage files

---
+++ script.js
@@ -0,0 +1,20 @@
+fetchData();
+async function fetchData(){
+  try{
+    const pokemonName = document.getElementById("pokemonname").value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if (!response.ok){
+        throw new Error ("could not fetch resource");
+    }
+
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonsprite");
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  }
+  catch(error){
+    console.error(error);
+  }
+}
